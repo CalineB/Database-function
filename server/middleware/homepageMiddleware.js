94a57564cc78891f313dc1b0b6f7aea13828dc82
Caline BRUNO <b.caline@outlook.com>
2025-01-01 18:32:00 +0100

    connection

--- server/middleware/homepageMiddleware.js
+++ server/middleware/homepageMiddleware.js
@@ -2,9 +2,9 @@ const { getVerses } = require('../database/pool');
 
 const versesMiddleware = async (req, res, next) => {
     try {
-        const verses = await getVerses(); // Récupérez les versets
-        req.verses = verses; // Attachez les versets à l'objet req
-        next(); // Passez au middleware ou à la route suivante
+        const verses = await getVerses();
+        req.verses = verses;
+        next(); 
     } catch (err) {
         console.error('Erreur lors de la récupération des versets', err);
         return res.status(500).send('Erreur interne du serveur');
